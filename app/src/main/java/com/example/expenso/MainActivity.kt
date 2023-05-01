009eff8dd79c3eb4d9e2f80d66fc8c0739d7346c
Ray-Palmer <sudeerachandrajith1212@gmail.com>
2023-05-01 15:05:26 +0530

    a

--- app/src/main/java/com/example/expenso/MainActivity.kt
+++ app/src/main/java/com/example/expenso/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.expenso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        print('k')
     }
 }
 
